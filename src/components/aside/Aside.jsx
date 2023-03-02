d28a0072ab6a15ae4f70f9ce2ab1b619941fde61
Samuel <you@example.com>
2023-03-02 23:53:10 +0100

    layout ok et avec animation, debut de la page home

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,19 +1,74 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
 
-import "./aside.scss"
+import "./aside.scss";
 
-const Aside = () => {
-    return (
-        <div className='aside-main'>
-            <h1>SAMUEL CHOJNACKI</h1>
-            <ul>
-                <li>home</li>
-                <li>series</li>
-                <li>film</li>
-                <li>about</li>
-            </ul>
-        </div>
-    );
+const Aside = (props) => {
+  const { openMenu, isMobile } = props;
+
+  const variant = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    open: {
+      x: 0,
+      opacity: 1,
+
+      transition: {
+        duration: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate={!isMobile || openMenu ? "open" : "initial"}
+      variants={variant}
+      className="aside-main"
+    >
+      <motion.h1 variants={variant}>SAMUEL CHOJNACKI</motion.h1>
+      <motion.p variants={variant}>Model and Comedian</motion.p>
+      <motion.ul>
+        <motion.li
+          variants={variant}
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          home
+        </motion.li>
+        <motion.li
+          variants={variant}
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          series
+        </motion.li>
+        <motion.li
+          variants={variant}
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          film
+        </motion.li>
+        <motion.li
+          variants={variant}
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          about
+        </motion.li>
+      </motion.ul>
+    </motion.div>
+  );
 };
 
 export default Aside;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./home.scss";
 
-const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Home = (props) => {
+
+  return (
+    <div className="home-main">
+      <h1>Hello My Name is Samuel. </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, quis!
+        Nam itaque possimus, iusto nostrum quaerat omnis quos commodi cum fugit
+        distinctio eos sint voluptatibus esse optio nulla accusantium debitis?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem facilis
+        molestiae suscipit asperiores rem cupiditate unde omnis expedita
+        provident, voluptates, voluptas quod dolores deleniti minima ea natus
+        odit recusandae. Optio? Expedita assumenda dolorem animi error
+        provident? Ab sapiente voluptates sint accusamus! Ratione commodi iure
+        illo harum corporis amet beatae fugiat, dolores a, molestiae maiores
+        dolorum modi voluptas nihil! Voluptates, quod! Amet ducimus tempora
+        cupiditate repellat. Consequatur quidem repellat provident consectetur
+        voluptates dolorum doloribus sit laudantium aperiam recusandae odit,
+        omnis quibusdam fugit mollitia excepturi, maiores voluptatibus! Esse
+        magni laborum dolores. Atque. Sequi voluptas modi praesentium nihil
+        delectus, officiis quos cumque tempora neque laudantium consequatur.
+        Minus possimus neque ipsum beatae voluptates, consequuntur earum culpa
+        perferendis quidem maxime repellat aliquam id voluptatem. Accusantium.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
