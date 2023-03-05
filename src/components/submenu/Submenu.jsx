a6dd1a53a0dc6f6f3f2ae3da790bda195f2c38ec
Samuel <you@example.com>
2023-03-05 03:15:50 +0100

    ajout de chevron sur aside

--- src/components/submenu/Submenu.jsx
+++ src/components/submenu/Submenu.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { MenuContext } from "./../../containers/layout/Layout";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { FiChevronDown, FiMoreHorizontal } from "react-icons/fi";
 
 const Submenu = ({ item, variant }) => {
   const { close, openMenu, isMobile, showList, setShowList } =
@@ -26,6 +27,7 @@ const Submenu = ({ item, variant }) => {
       <>
         <motion.p variants={variant} layout onClick={toggleList}>
           {item.title}
+          {showList === item.title.toLowerCase() ? <FiMoreHorizontal/> : <FiChevronDown/>}
         </motion.p>
         <motion.ul layout>
           {item.submenu.map((sub, index) => {
